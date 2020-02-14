fb68e46589cf996d4c8f93c1425778dffa6c980c
Ms_Noxolo <KheswaNL925@gmail.com>
2020-02-14 15:27:05 +0200

    Update function7.py
    Added the comments and code to function 7

---
+++ Eskom Functions/function7.py
@@ -0,0 +1,11 @@
+def stop_words_remover(df):
+    stopwords = stop_words_dict['stopwords']
+    Tweets_split = df['Tweets'].apply(lambda x: x.lower().split()) # Tokeising the sentences are according to the definition in function 6
+    no_stopwords = []  # Creating  a list to place the setences without the stopwords
+    for item in Tweets_split:
+        for stopword in stopwords:
+            if stopword in item:
+                item.remove(stopword)  # Creating  a list to place the setences without the stopwords
+        no_stopwords.append(item) # Adding the setences to the created list
+    df.insert(loc=2, column='Without Stop Words', value=no_stopwords)
+    return df
