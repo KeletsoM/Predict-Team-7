a53504d185f8da7df7004c204414955ac1830e4c
thuthumvelo25 <thuthumvelo25@gmail.com>
2020-02-17 09:04:18 +0200

    Update function2.py
    I addednfunction 2 which simply returns the five number summary of a given data, the five number summary will always be in second decimal place.

---
+++ Eskom Functions/function2.py
@@ -0,0 +1,10 @@
+def five_num_summary(items):
+    
+    import numpy as np
+    items = np.array(items)
+    
+    h = {'max': round(np.max(items),2), 'median': round(np.median(items),2), 'min': round(np.min(items),2), 'q1': round(np.quantile(items,0.25),2), 'q3': round(np.quantile(items,0.75),2)}
+    
+    return h
+    
+    pass
