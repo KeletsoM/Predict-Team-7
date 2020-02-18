be33b37bb751bc66576605621891e9337623fcc7
Keletso <u14062552@tuks.co.za>
2020-02-18 17:05:47 +0200

    Tests update

--- EskomFunctions/function7.py
+++ EskomFunctions/function7.py
@@ -1,5 +1,5 @@
 def stop_words_remover(df):
-''' This function which removes english stop words from a tweet.'''
+    ''' This function which removes english stop words from a tweet.'''
     stopwords = stop_words_dict['stopwords'] # Extracting the stopwords from the stopwords dict
     Tweets_split = df['Tweets'].apply(lambda x: x.lower().split()) # Tokeising the sentences are according to the definition in function 6
     no_stopwords = Tweets_split.apply(lambda x: [item for item in x if item not in stopwords]) # Removing stopwords from each tweet list using the lambda function, then assinging this to a 'no_stopwords' variable
--- tests/test5.py
+++ tests/test5.py
@@ -1,3 +1,10 @@
 ''' Test for function 5'''
 
+from EskomFunctions.function5 import number_of_tweets_per_day
+import pandas as pd
+twitter_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/twitter_nov_2019.csv'
+twitter_df = pd.read_csv(twitter_url)
+twitter_df.head()
+
+
 number_of_tweets_per_day(twitter_df.copy())
