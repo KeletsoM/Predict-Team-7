33dd580e27178f62037763f2224e4c64344c81fa
Keletso <u14062552@tuks.co.za>
2020-02-18 09:02:34 +0200

    Update function5.py

--- Eskom Functions/function5.py
+++ Eskom Functions/function5.py
@@ -4,3 +4,7 @@ def number_of_tweets_per_day(df):
 
     
     d = df['Date'].str.split(' ', expand = True) # splits the date datapoints into date & time
+    df['Date'] = d.iloc[:,0]  # takes the date coloumn
+    c = df.groupby('Date').count() # this line groups the data by the date and counts the number of tweets in a given day
+
+    return c
