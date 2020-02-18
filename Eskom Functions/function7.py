13cc3ab491642a5f16adf4f52271263e58301db8
Ms_Noxolo <KheswaNL925@gmail.com>
2020-02-18 14:41:53 +0200

    Update function7.py
    Reworked function 7 using lambda as a for looped returned a failed test for index 9 in the autograder

--- Eskom Functions/function7.py
+++ Eskom Functions/function7.py
@@ -1,12 +1,7 @@
 def stop_words_remover(df):
 ''' This function which removes english stop words from a tweet.'''
-    stopwords = stop_words_dict['stopwords']
+    stopwords = stop_words_dict['stopwords'] # Extracting the stopwords from the stopwords dict
     Tweets_split = df['Tweets'].apply(lambda x: x.lower().split()) # Tokeising the sentences are according to the definition in function 6
-    no_stopwords = []  # Creating  a list to place the setences without the stopwords
-    for item in Tweets_split:
-        for stopword in stopwords:
-            if stopword in item:
-                item.remove(stopword)  # Creating  a list to place the setences without the stopwords
-        no_stopwords.append(item) # Adding the setences to the created list
-    df.insert(loc=2, column='Without Stop Words', value=no_stopwords)
+    no_stopwords = Tweets_split.apply(lambda x: [item for item in x if item not in stopwords]) # Removing stopwords from each tweet list using the lambda function, then assinging this to a 'no_stopwords' variable
+    df.insert(loc=2, column='Without Stop Words', value=no_stopwords) # Adding the tweets withouth stopwords as a column to the DataFrame
     return df
