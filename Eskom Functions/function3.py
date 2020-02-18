a615147a5e695cd34164dd685e1336ce105f2834
Hlamulo-Mav <60342472+Hlamulo-Mav@users.noreply.github.com>
2020-02-18 12:54:01 +0200

    Update function3.py

--- Eskom Functions/function3.py
+++ Eskom Functions/function3.py
@@ -1,7 +1,7 @@
 def date_parser(dates):
-    """ initialize an empty list"""
-    c = [] 
-    for i in dates:
-        i = i[:10] 
-        c.append(i) 
-    return c
+    """ returns a list of strings containing only dates"""
+    c = []  # initialize an empty list
+    for i in dates: 
+        i = i[:10]  # the date from the datetime string is only made up of 9 characters
+        c.append(i)  # Adds items to the list c
+    return c        #return list c
