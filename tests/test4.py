75939e322c7d0ca1ef0800ef5776be3d69fe1bf1
Keletso <u14062552@tuks.co.za>
2020-02-18 14:49:19 +0200

    Update test4.py

---
+++ tests/test4.py
@@ -0,0 +1,5 @@
+twitter_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/twitter_nov_2019.csv'
+twitter_df = pd.read_csv(twitter_url)
+twitter_df.head()
+
+extract_municipality_hashtags(twitter_df.copy())
