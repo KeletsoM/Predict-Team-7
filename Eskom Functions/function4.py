340d39d706bc7dc55fecf7ee3e4148c631b3c9bc
Keletso <u14062552@tuks.co.za>
2020-02-18 14:43:26 +0200

    Update function4.py

--- Eskom Functions/function4.py
+++ Eskom Functions/function4.py
@@ -2,6 +2,17 @@
 
 
 def extract_municipality_hashtags(df):
+    
+
+    ''' This function takes a twitter dataframe as an input then the output is the dataframe with 2 new columns namely a hashtag 
+    column and a municipality column.
+
+    for example: if the tweet contains the @mention '@CityPowerJhb' then the coresponding output in the municipality column should be 
+    Johannesburg. 
+
+    The function also extracts hashtags and saves them under the hastags column as a list in the dataframe.
+    
+    '''
 
     import pandas as pd
     import numpy as np
@@ -16,7 +27,7 @@ def extract_municipality_hashtags(df):
     '@CityTshwane' : 'Tshwane'
         }
 
-    g = df['Tweets'].str.findall(r'@.*?(?=\s|$)')
+    g = df['Tweets'].str.findall(r'@.*?(?=\s|$)') # finds all @mentions 
     df['municipality'] = g.apply(lambda x: [mun_dict[g] for g in mun_dict.keys() if g in x])
     df["municipality"]= df['municipality'].apply(lambda x: ''.join(x) if len(x) > 0 else np.nan)
     df['hashtags'] = df['Tweets'].str.findall(r'#.*?(?=\s|$)')
