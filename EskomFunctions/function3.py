63d27f84d7b3f2d6d747ef34c092ed77a7444b6d
Hlamulo-Mav <60342472+Hlamulo-Mav@users.noreply.github.com>
2020-02-19 09:34:21 +0200

    Update function3.py

--- EskomFunctions/function3.py
+++ EskomFunctions/function3.py
@@ -1,5 +1,5 @@
 def date_parser(dates):
-    """ returns a list of strings containing only dates"""
+    """ return a list of strings where each element in the returned list contains only the date"""
     c = []  # initialize an empty list
     for i in dates: 
         i = i[:10]  # the date from the datetime string is only made up of 9 characters
