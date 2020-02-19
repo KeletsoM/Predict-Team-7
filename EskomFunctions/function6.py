62a47a7d17063d2177eb62b38865b65e7a0555e4
Hlamulo-Mav <60342472+Hlamulo-Mav@users.noreply.github.com>
2020-02-19 12:07:03 +0200

    Update function6.py

--- EskomFunctions/function6.py
+++ EskomFunctions/function6.py
@@ -1,6 +1,14 @@
 ### START FUNCTION
 def word_splitter(df):
     
+""" This function takes pandas dataframe as input, split the sentence into a list of separate words and returns a modified dataframe
+
+Example 
+----------
+
+words_splitter(twitter_df.copy()).loc[0, "Split Tweets"] == ['@bongadlulane', 'please', 'send', 'an', 'email', 'to', 'mediadesk@eskom.co.za']
+"""
+    
     # initializing char to increment through the data frame with
     char = 0
     
